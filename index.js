0bffaf6c0abe284bc71906092a0d2dd184fa5c05
Carolaine dos Santos <100321745+carolainesantos@users.noreply.github.com>
2024-01-08 15:30:34 -0300

    Finalizando exercicio 6 da lista

--- index.js
+++ index.js
@@ -46,6 +46,45 @@ app.get("/api/exercicio3", (req, res) => {
   res.json({ resultado });
 });
 
+/* Ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit. A fórmula de conversão é : F = (9*C+160)/5 Sendo F a temperatura em Fahrenheit e C a temperatura em Celsius.*/
+app.get("/api/exercicio4", (req, res) => {
+  const celsius = parseFloat(req.query.celsius);
+  const resultado = (9 * celsius + 160) / 5;
+
+  res.json({ resultado });
+});
+
+/* FUP que solicite a quantidade de milhas e converta para quilometros (1milha = 1.60934kms)*/
+app.get("/api/exercicio5", (req, res) => {
+  const milhas = parseFloat(req.query.milhas);
+  const resultado = milhas * 1.609;
+
+  res.json({ resultado });
+});
+
+/* FUP que solicite a duração de um evento em segundos e imprima em horas, minutos e segundos */
+app.get("/api/exercicio6", (req, res) => {
+  const duracao = parseFloat(req.query.duracao);
+  const horas = Math.floor(duracao / 3600);
+  const minutos = Math.floor((duracao % 3600) / 60);
+  const segundos = duracao % 60;
+
+  const resultado = { horas, minutos, segundos };
+
+  res.json({ resultado });
+});
+
+/*
+app.get("/api/exercicio6", (req, res) => {
+  const duracao = parseFloat(req.query.duracao);
+  const hr = parseFloat(req.query.hr);
+  const min = parseFloat(req.query.min);
+  const seg = parseFloat(req.query.seg);
+  const resultado = (min = duracao / 60);
+
+  res.json({ resultado });
+}); */
+
 app.listen(3000, () => {
   console.log("Servidor rodando na porta 3000");
 });
